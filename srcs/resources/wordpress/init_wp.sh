ceeeb4c18a762b1b4e6cb89f488fc271d010f46a
jmartos- <jcmv.42malaga@gmail.com>
2024-12-17 23:01:45 +0100

    keep going

--- srcs/resources/wordpress/init_wp.sh
+++ srcs/resources/wordpress/init_wp.sh
@@ -7,4 +7,6 @@ sed -i "s/username_here/$MARIADB_USER/g" /var/www/html/wp-config.php
 sed -i "s/password_here/$MARIADB_PASS/g" /var/www/html/wp-config.php
 sed -i "s/localhost/$MARIADB_NAME/g" /var/www/html/wp-config.php
 
-echo "< FIN DE ARCHIVO DE CONFIGURACION DE WORDPRESS >"
+php-fpm7.4 -y /etc/php/7.4/fpm/php-fpm.conf -F
+
+echo "< SI VES ESTE ECHO ES QUE NO SE HA INICIADO PHP... >"
