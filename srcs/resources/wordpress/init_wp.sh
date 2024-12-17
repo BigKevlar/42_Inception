60b3d053aaaccc97f6be2a6e47fbbca35b15d873
jmartos- <jcmv.42malaga@gmail.com>
2024-12-18 00:31:24 +0100

    keep going

--- srcs/resources/wordpress/init_wp.sh
+++ srcs/resources/wordpress/init_wp.sh
@@ -7,6 +7,13 @@ sed -i "s/username_here/$MARIADB_USER/g" /var/www/html/wp-config.php
 sed -i "s/password_here/$MARIADB_PASS/g" /var/www/html/wp-config.php
 sed -i "s/localhost/$MARIADB_NAME/g" /var/www/html/wp-config.php
 
+wp core install --allow-root --path=/var/www/html \
+	--url=$DOMAIN_NAME --title=inception \
+	--admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS \
+	--admin_email=$WP_ADMIN_MAIL
+
+wp user create $WP_USER_USER $WP_USER_MAIL --user_pass=$WP_PASS --allow-root --role=author --path=/var/www/html
+
 php-fpm7.4 -y /etc/php/7.4/fpm/php-fpm.conf -F
 
 echo "< SI VES ESTE ECHO ES QUE NO SE HA INICIADO PHP... >"
